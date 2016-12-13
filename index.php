83bc58d2c156ae2bcb4736512fa4bbc79c9a6147
yito2 <yito2@u.rochester.edu>
2016-12-13 06:12:43 -0500

    Index Z-formation install, Portfolio JS install

--- index.php
+++ index.php
@@ -2,13 +2,104 @@
 	$pageTitle = "Homepage"; 
 	include "inc/top-part.inc";
 ?>
+		<div class="border">
+			<img class="right logo" src="images/yito.jpg" alt="yukako">
+			<h2>Yukako Louise Ito</h2>
+			<h3 id="subtitle">Incoming Market Data Analyst at Bloomberg LP<br/>
+				University of Rochester, Class of 2017</h3>
+			<p>
+				Phone: (314) 623-9157 <br/>
+				Email: yito2@u.rochester.edu <br/>
+				LinkedIn Profile: http://tiny.cc/k0zhwx
+			</p>
 
-<?php include "inc/nav.inc" ?>
+			<p>
+				This Saint Louis native and soon-to-be University of Rochester graduate excels at computing 
+				and creative thinking. She is focused on applying technical knowledge to a business environment, 
+				in hopes of combining analytics and logistics with marketing and technology. 
+			</p>
 
-	<div>
-		<img class="right img-frame" src="images/yito.jpg" alt="yukako">
-		<p>
-			long scrolly homepage that features	your accomplishments (like a	résumé)</p>
+			<p class="title">
+				Dancer, Netflix Enthusiast, Ex-Competitive Figure Skater
+			</p>
+		</div>
+
+		<div class="resume">
+
+			<div class="top-section">
+				<p class="works-title">Works</p>
+				<div class="works-section">
+					<a href="http://urcheer.wix.com/ur-cheer" target="_blank"><img class="left icon" src="images/cheer-front.jpg" alt="yukako"></a>
+					<p>
+						<span class="title">University of Rochester Cheerleading Website</span><br/>
+						Creator, Manager. This official site was created and designed to further the public knowledge of our team and our goals. Publishing our motivations, updates, and schedules. 
+					</p>
+					<p>
+						Click the image to learn more.
+					</p>
+				</div>
+
+				<div class="works-section">
+					<a href="http://sdtbetapi.wixsite.com/sigmadeltatau" target="_blank"><img class="left icon" src="images/sdt-front.jpg" alt="yukako"></a>
+					<p>
+						<span class="title">University of Rochester Sigma Delta Tau Website</span><br/>
+						Creator. This official website was created after my installation was the chapter's webmistress. The information about sisters, our philanthropies, and information on formal recruitment is showcased and presented.
+					</p>
+					<p>
+						Click the image to learn more.
+					</p>
+				</div>
+			</div>
+
+			<div class="sections">
+				<p class="title">Education, Awards, Affiliations</p>
+				<p>University of Rochester, Hajim School of Engineering and Applied Sciences (Rochester, NY) <br/>
+					Anticipated graduation: May 2017
+						<ul>
+							<li>Bachelor of Arts in Computer Science, Minors in Business and Dance</li>
+							<li>Dean’s Scholarship Award for Academic Achievement and Rochester National Grant recipient</li> 
+							<li>Captain and Public Relations/Recruitment Chair of the University of Rochester Cheerleading Squad</li>
+							<li>Standards Representative of Sigma Delta Tau sorority - Beta Pi chapter</li>
+							<li>Member of the University of Rochester Women in Computing Organization</li>
+						</ul>
+				</p>
+				<p>Mary Institute and Saint Louis Country Day School (Saint Louis, MO)<br/>
+					September 2006 - June 2013</p>
+				<p>Saint Louis Japanese School (Saint Louis, MO)<br/>
+					April 1999 - March 2010</p>
+			</div>
+
+			<div class="sections">
+				<p class="title">Technical Qualifications</p>
+				<p>
+					Applications:
+						<ul>
+							<li>Google Analytics</li>
+							<li>Adobe (Photoshop, InDesign, Dreamweaver, Illustrator)</li>
+						</ul> 
+					Programming Languages:
+						<ul>
+							<li>HTML</li>
+							<li>CSS</li>
+							<li>JavaScript</li>
+							<li>Java</li>
+						</ul>
+				</p>
+			</div>
+
+			<div class="sections">
+				<p class="title">Other Qualifications</p>
+				<p>
+					Spoken and Written Language:
+						<ul>
+							<li>English</li>
+							<li>Japanese</li>
+							<li>Elementary-level French</li>
+						</ul>
+				</p>
+			</div>
+
+		</div>
 	</div>
 
 <?php include "inc/footer.inc" ?>
--- portfolio.php
+++ portfolio.php
@@ -3,12 +3,12 @@
 	include "inc/top-part.inc";
 ?>
 
-<?php include "inc/nav.inc" ?>
-
 	<div>
-		<h2>Works</h2>
-		<img class="right img-frame" src="images/**.jpg" alt="link">
-		<p>a	portfolio	page	with	thumbnails	and	links	of	your	online	accomplishments1</p>
+		<h2>Portfolio</h2>
+		<div class="fotorama">
+  			<img src="http://s.fotorama.io/1.jpg">
+  			<img src="http://s.fotorama.io/2.jpg">
+		</div>
 	</div>
 
 <?php include "inc/footer.inc" ?>
